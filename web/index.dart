d9ecff56a5042820ce868900235fae6e3994d845
Mike Lewis <mike@updroid.com>
2015-12-16 19:16:14 -0800

    web: fixed script path

--- web/index.dart
+++ web/index.dart
@@ -4,7 +4,7 @@ import 'editor.dart';
 void main() {
   ScriptElement aceJs = new ScriptElement()
     ..type = 'text/javascript'
-    ..src = 'tabs/upcom-editor/src-min-noconflict/ace.js';
+    ..src = 'plugins/upcom-editor/src-min-noconflict/ace.js';
   document.body.children.add(aceJs);
 
   aceJs.onLoad.first.then((_) {
